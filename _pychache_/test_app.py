c4f1d4ac6784b28666408be557a932fa74e71e9b
sebastian-helguero <102705594+sebastian-helguero@users.noreply.github.com>
2025-05-09 20:17:17 -0300

    Update test_app.py

--- _pychache_/test_app.py
+++ _pychache_/test_app.py
@@ -4,6 +4,7 @@ def maximo_basico(a, b):
         return a
     return b
 
-
-assert maximo_basico(10, 5) == 10
-assert maximo_basico(9, 18) == 18
+def test_maximo_basico():
+    assert maximo_basico(1, 2) == 2
+    assert maximo_basico(10, 5) == 10
+    assert maximo_basico(9, 18) == 18
